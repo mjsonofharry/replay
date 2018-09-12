69ef3f40eaa9200bc1cc7dfe95a77ffc069b7eb4
mjharrison <mjsonofharry@gmail.com>
2018-09-12 17:31:44 -0400

    removed redundant perf tests

--- tests/test_replay.py
+++ tests/test_replay.py
@@ -149,23 +149,9 @@ class TestFrameData:
         assert len(result.keys()) == 717
         assert result[1] == [Action.ANGLES_ENABLED]
         assert result[2385] == [0]
-    
-    def test_get_lookup_table_performance_long(self, benchmark):
-        result = benchmark(lambda: FrameData.get_lookup_table(PlayerData.get_frame_data(ReplayData.get_player_data(SAMPLE_REPLAY_DATA)[0])))
-        assert isinstance(result, dict)
-        assert len(result.keys()) == 717
-        assert result[1] == [Action.ANGLES_ENABLED]
-        assert result[2385] == [0]
 
     def test_get_state_table_performance(self, benchmark):
         result = benchmark(FrameData.get_state_table, PlayerData.get_frame_data(ReplayData.get_player_data(SAMPLE_REPLAY_DATA)[0]))
-        assert isinstance(result, dict)
-        assert len(result.keys()) == 717
-        assert isinstance(result[1], list)
-        assert result[1][0] == False
-
-    def test_get_state_table_performance_long(self, benchmark):
-        result = benchmark(lambda: FrameData.get_state_table(PlayerData.get_frame_data(ReplayData.get_player_data(SAMPLE_REPLAY_DATA)[0])))
         assert isinstance(result, dict)
         assert len(result.keys()) == 717
         assert isinstance(result[1], list)
