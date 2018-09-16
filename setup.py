6b743da3068854c6dc8fe3389ba01391195ebc86
mjharrison <mjsonofharry@gmail.com>
2018-09-15 23:10:03 -0400

    setup :)

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='Replay',
+    version='0.1dev',
+    description='A functional API that uses regular expressions to parse replay (.roa) files from Rivals of Aether.',
+    author='Matthew James Harrison',
+    url='https://github.com/mjharrison/replay',
+    license='GNU General Public License v3.0',
+    packages=[
+        'replay'
+    ],
+)
